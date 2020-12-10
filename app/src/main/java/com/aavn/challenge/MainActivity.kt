b2ce7fcce43388ff71786befae21e6c11aeb5461
Dang Duc Nam <nam.dang@axonactive.com>
2020-12-10 11:41:33 +0700

    no message

---
+++ app/src/main/java/com/aavn/challenge/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.aavn.challenge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.aavn.sessions.Sessions
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startSessions(view: View) {
+        val intent = Intent(this, Sessions::class.java);
+        startActivity(intent)
+    }
+}
---
+++ sessions/src/main/java/com/aavn/sessions/Sessions.kt
@@ -0,0 +1,11 @@
+package com.aavn.sessions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sessions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sessions)
+    }
+}
