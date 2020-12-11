f3a692a169109ed0ce2e0d30f972029d145dd481
Dang Duc Nam <nam.dang@axonactive.com>
2020-12-11 12:17:08 +0700

    no message

--- app/src/main/java/com/aavn/challenge/MainActivity.kt
+++ app/src/main/java/com/aavn/challenge/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import com.aavn.sessions.Sessions
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
